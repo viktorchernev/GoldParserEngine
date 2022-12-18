17c174981b66fcf7bf67dde3ca3d9ef035282028
Viktor Chernev <viktorchernev@abv.bg>
2022-12-18 20:08:50 +0200

    Add files via upload

---
+++ GoldParserEngine/GoldParserEngine/Helpers/GrammarToText.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoldParser.Grammar;
+
+namespace GoldParser.Helpers
+{
+    public static class GrammarToText
+    {
+        public static string productionToText(GrammarProduction production, bool alwaysDelimitTerminals)
+        {
+            return production.Head.ToString() + " ::= " + symbolListToString(production.Handle, " ", alwaysDelimitTerminals);
+        }
+        public static string symbolListToString(List<GrammarSymbol> list, string separator, bool alwaysDelimitTerminals)
+        {
+            string result = "";
+            for (int n = 0; n <= list.Count - 1; n++)
+            {
+                GrammarSymbol symbol = list[n];
+                result += (n == 0 ? "" : separator) + symbol.ToString(alwaysDelimitTerminals);
+            }
+
+            return result;
+        }
+
+
+        public static string symbolToText(GrammarSymbol symbol, bool alwaysDelimitTerminals)
+        {
+            switch (symbol.Type)
+            {
+                case GrammarSymbolType.Nonterminal:
+                    return "<" + symbol.Name + ">";
+                case GrammarSymbolType.Terminal:
+                    return symbolFormatLiteral(symbol.Name, alwaysDelimitTerminals);
+                default:
+                    return "(" + symbol.Name + ")";
+            }
+        }
+        public static string symbolFormatLiteral(string source, bool forceDelimit)
+        {
+            if (source == "'") return "''";
+
+            for (int i = 0; i < source.Length && !forceDelimit; i++)
+            {
+                char ch = source[i];
+                forceDelimit = !(char.IsLetter(ch) || ch == '.' || ch == '_' || ch == '-');
+            }
+
+            if (forceDelimit) return "'" + source + "'";
+            return source;
+        }
+    }
+}
---
+++ GoldParserEngine/GoldParserEngine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoldParser.Helpers;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace GoldParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+		{
+            //https://github.com/meziantou/GoldParser-Engine/
+
+            string filepath = @"C:\Users\vikto\Desktop\A.egt";
+
+            //string toparse = @"C:\Users\vikto\Desktop\PORT\smartphone-locks.ds";
+            //Parser parser = new Parser();
+            //parser.LoadTables(filepath);
+            //parser.Parse();
+
+            string json = GrammarToJSON.ReadFile(filepath);
+            bool breakpointer = true;
+		}
+
+
+		//structure = new CGTStructure(header, receaords);
+		//content = new CGTContent(structure);
+		//dfaStates = CreateDFAStates(content);
+		//parserStates = CreateParserStates(content);
+	}
+}
